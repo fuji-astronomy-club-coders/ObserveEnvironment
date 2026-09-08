d99b190eb8b96cfcc50a2e34d9c189f6f94ccbae
めんこんこば <Arikui57@outlook.jp>
2026-09-08 13:28:06 +0000

    refactor:extractmetadata関数の修正: メタデータの抽出処理を改善し、変数名を統一

--- core/extracter.py
+++ core/extracter.py
@@ -1,22 +1,28 @@
-
 from pathlib import Path
 
-def extractMetadata(path:Path) -> dict :
-    with open(str(Path),'r') as f:
-        lines = f.readline()
+from utils import loadCsv2dList, save2dListCsv
 
-    metadata = {}
+
+def extractmetadata(path:Path) -> dict :
+    """
+    asiの作成するtxtファイルからメタデータをdict形式で抽出します
+    """
+    with open(str(path), 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    extracted_metadata = {}
 
     for i,line in enumerate(lines):
         if "=" not in line:
             print(f"無効な行としてskipします L{i}: {line}")
         else:
             item,value =line.split("=")
-            metadata[item.strip()] = value.strip()
+            extracted_metadata[item.strip()] = value.strip()
 
-    return metadata
+    return extracted_metadata
 
 if "__main__" == __name__ :
     filepath = Path(r"samples/01-17-LTsertext/2026-01-17-0203_9-CapObj.ser.txt")
     from pprint import pprint
-    pprint(extractMetadata(filepath))
+    cr_metadata= extractmetadata(filepath)
+    pprint(cr_metadata)
