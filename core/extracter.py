92f22aafddac0410fd1df3756a7ef4c4d8e69cc4
めんこんこば <Arikui57@outlook.jp>
2026-09-08 13:28:28 +0000

    feat: メタデータをCSV形式で保存する機能を追加

--- core/extracter.py
+++ core/extracter.py
@@ -21,8 +21,47 @@ def extractmetadata(path:Path) -> dict :
 
     return extracted_metadata
 
+def savemetadata(metadata:dict, output:Path) -> None :
+    """
+    既存または新しいcsvにメタデータを書き込みます
+    """
+    clm,lin = 1,0
+    transed_data=[[]]    
+    exs_headers = ["ID"]
+    if output.exists() and output.is_file():
+        existing = loadCsv2dList(output)
+        if existing : 
+            exs_headers = existing[0]
+            clm = len(exs_headers)
+            exs_data=existing[1:]
+            lin = len(exs_data)
+            transed_data=[list(row) for row in zip(*exs_data)]
+
+    metadata["ID"] = lin + 1
+
+    new_headers = [k for k in metadata.keys() if k not in exs_headers]
+    headers = exs_headers + list(new_headers)
+
+    print("header",headers)
+    new_clms = [[""] * lin for _ in range(len(headers) - clm)]
+    data =  transed_data + new_clms
+    for i,h in enumerate(headers):
+        val = metadata.get(h,"")
+        print("val",val)
+        data[i].append(val)
+
+    print("data",data)
+    table = [headers] + [list(row) for row in zip(*data)]
+    print("table",table)
+    save2dListCsv(table,output)
+
 if "__main__" == __name__ :
     filepath = Path(r"samples/01-17-LTsertext/2026-01-17-0203_9-CapObj.ser.txt")
     from pprint import pprint
     cr_metadata= extractmetadata(filepath)
     pprint(cr_metadata)
+
+    outfilename = str(filepath.name.split(".",1)[0])+".csv"
+    outputdir = Path("samples") / "result" / "metadata" 
+    outputdir.mkdir(parents=True, exist_ok=True)
+    savemetadata(cr_metadata,outputdir/outfilename)
